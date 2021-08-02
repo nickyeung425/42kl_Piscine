4a6861876b0d2749e86a0ce3bca4e784e96196a0
Nick Yeung <nickyeung425@hotmail.com>
2021-08-02 19:51:50 +0800

    Test files

---
+++ test/c00/main00.c
@@ -0,0 +1,8 @@
+void ft_putchar(char c);
+int main(void)
+{
+  ft_putchar('A');
+  ft_putchar('B');
+  ft_putchar('C');
+  return (0);
+}
---
+++ test/c00/main07.c
@@ -0,0 +1,7 @@
+void ft_putnbr(int n);
+
+int main(void)
+{
+  ft_putnbr(500);
+  return (0);
+}

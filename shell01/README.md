4e37de8f2d75005ddd460d885129ae5e576474db
Nick Yeung <nickyeung425@hotmail.com>
2021-08-03 19:07:28 +0800

    Fixed error for first submit

---
+++ shell01/README.md
@@ -0,0 +1,4 @@
+ex01 -new line at end of file
+ex03 remove -type f
+ex05 no new line at end of file and chmod
+ex07 no new line at end of line
--- shell01/ex07/r_dwssap.sh
+++ shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v "^#" | awk "NR % 2 == 0" | awk -F ":" '{print $1}' | rev | sort -r | awk "FNR>=$FT_LINE1 && FNR<=$FT_LINE2" | sed 's/$/,/' | tr '\n' ' ' | sed 's/..$//' 
+cat /etc/passwd | grep -v "^#" | awk "NR % 2 == 0" | awk -F ":" '{print $1}' | rev | sort -r | awk "FNR>=$FT_LINE1 && FNR<=$FT_LINE2" | sed 's/$/,/' | tr '\n' ' ' | sed 's/..$//' | sed 's/$/./'

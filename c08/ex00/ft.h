1ca028613d0e588845c706a97048c3b67c3c3772
Nick Yeung <nickyeung425@hotmail.com>
2021-08-16 19:59:43 +0800

    done c08 ex00

---
+++ c08/ex00/ft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wyeung <wyeung@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 00:19:52 by wyeung            #+#    #+#             */
+/*   Updated: 2021/08/16 00:19:53 by wyeung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+$endif

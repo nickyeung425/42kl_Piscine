9e8e90c3feeb5e5fc8b60647fe793b06e4c41fa8
Nick Yeung <nickyeung425@hotmail.com>
2021-08-19 00:51:13 +0800

    Fixed bug for c03

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: wyeung <wyeung@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 19:19:59 by nick              #+#    #+#             */
-/*   Updated: 2021/08/18 20:19:29 by wyeung           ###   ########.fr       */
+/*   Updated: 2021/08/19 00:32:59 by wyeung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,16 @@ unsigned int	ft_str_len(char *str)
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	index_dest;
+	unsigned int	result_dest;
 	unsigned int	index_src;
+	unsigned int	result_src;
 
 	if (size < 1)
 		return (size + ft_str_len(src));
+	result_dest = ft_str_len(dest);
 	index_dest = ft_str_len(dest);
 	index_src = 0;
+	result_src = ft_str_len(src);
 	while (src[index_src] && index_dest < size - 1)
 	{
 		dest[index_dest] = src[index_src];
@@ -38,7 +42,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 		index_src++;
 	}
 	dest[index_dest] = '\0';
-	if (size < ft_str_len(dest))
-		return (size + ft_str_len(src));
-	return (ft_str_len(dest) + ft_str_len(src));
+	if (size < result_dest)
+		return (result_src + size);
+	return (result_src + result_dest);
 }

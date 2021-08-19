138232a0aab5911fb6220519286db02db54eaf5e
Nick Yeung <nickyeung425@hotmail.com>
2021-08-19 21:33:41 +0800

    Fixed bug about c06

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -1,16 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: wyeung <wyeung@student.42kl.edu.my>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/15 18:27:19 by wyeung            #+#    #+#             */
-/*   Updated: 2021/08/18 19:31:07 by wyeung           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   ft_sort_params.c                                   :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: wyeung <wyeung@student.42kl.edu.my>        +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2021/08/15 18:27:19 by wyeung            #+#    #+#             */
+// /*   Updated: 2021/08/19 21:13:07 by wyeung           ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
 #include <unistd.h>
+// #include <stdio.h>
 
 void	print_str(char *str)
 {
@@ -24,47 +25,29 @@ void	print_str(char *str)
 
 int	compare_str(char *s1, char *s2)
 {
-	while (*s1 != '\0' && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
-}
+	int i;
 
-int	find_lowest_string(int start, int end, char **str_array)
-{
-	int	lowest;
-	int	compare_result;
-	int	index;
-
-	index = start;
-	while (index < end)
-	{
-		compare_result = compare_str(str_array[start], str_array[index]);
-		if (compare_result >= 0)
-		{
-			lowest = index;
-		}
-		index++;
-	}
-	return (lowest);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
 }
 
 void	sort_params(int size, char **str_array)
 {
 	int		index;
-	int		lowest_index;
 	char	*temp;
 
 	index = 1;
-	while (index < size)
+	while (index < size - 1)
 	{
-		lowest_index = find_lowest_string(index, size, str_array);
-		temp = str_array[index];
-		str_array[index] = str_array[lowest_index];
-		str_array[lowest_index] = temp;
-		swap_str(index, lowest_index, str_array);
+		if (compare_str(str_array[index], str_array[index + 1]) > 0)
+		{
+			temp = str_array[index];
+			str_array[index] = str_array[index + 1];
+			str_array[index + 1] = temp;
+			index = 0;
+		}
 		index++;
 	}
 }
